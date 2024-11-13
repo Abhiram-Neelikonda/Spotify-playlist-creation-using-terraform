8498e9eb2af7033d0d75852d6d54717bbb3b0bc9
Abhiram-Neelikonda <84191846+Abhiram-Neelikonda@users.noreply.github.com>
2024-11-13 13:39:31 +0530

    Add files via upload

---
+++ playlist.tf
@@ -0,0 +1,6 @@
+resource "spotify_playlist" "Country_Music"{
+    name = "Country_Music"
+    description = "My Country music playlist"
+    public = true
+    tracks = ["1HbzxLqpNVPdiBXvpC7Ovb"]
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+api_key = "Oo9rWDiERcssRAx0KhK5RgbL5rU6ued5hsfIoi6wy2HJ5qyUUk177tlL3ngD0OUm"
+
+ 
